c51fdac365a9492472ab9c7e7ba8d764c956c175
REPNOXX <thisisrepnoxx@gmail.com>
2026-07-06 16:58:49 +0530

    Simplify: use eventlet + python server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import gevent.monkey
-gevent.monkey.patch_all()
+import eventlet
+eventlet.monkey_patch()
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit, join_room
